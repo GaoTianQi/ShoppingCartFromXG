59f9396c5e2230219921d26776c0d5a31fa273aa
GaoTianQi <273071877@qq.com>
2015-12-09 20:30:32 +0800

    day01 homework

---
+++ ShoppingCart/ShoppingCart/Model/XGGoodModel.swift
@@ -0,0 +1,50 @@
+//
+//  XGGoodModel.swift
+//  ShoppingCart
+//
+//  Created by ios－54 on 15/12/9.
+//  Copyright © 2015年 XiaoGao. All rights reserved.
+//
+
+import UIKit
+
+class XGGoodModel: NSObject {
+    
+    // 是否已经加入购物车
+    var alreadyAddShoppingCart: Bool = false
+    
+    // 商品图片名称
+    var iconName: String?
+    
+    // 商品标题
+    var title: String?
+    
+    // 商品描述
+    var desc: String?
+    
+    // 商品购买个数，默认0
+    var count: Int = 0
+    
+    // 新价格
+    var newPrice: String?
+    
+    // 老价格
+    var oldPrice: String?
+    
+    // 是否选中，默认没有选中
+    var selected: Bool = true
+    
+    // 字典转模型
+    init(dict: [String : AnyObject]) {
+        super.init()
+        
+        // 使用kvc为当前对象属性设置值
+        setValuesForKeysWithDictionary(dict)
+    }
+    
+    // 防止对象属性和kvc时的dict的key不匹配二崩溃
+    override func setValue(value: AnyObject?, forUndefinedKey key: String) {
+        
+    }
+
+}
